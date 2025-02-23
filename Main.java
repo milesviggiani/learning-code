0bd5d6de65d81b77dea1158afabc22eaa6d5571f
Chuckles62 <115182323+chucky123455@users.noreply.github.com>
2025-02-23 21:08:11 +0000

    Change

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main(String[] args){
-barbarian idiot = new barbarian(20, 1, 5, "GreatAxe", AvaliableArmour.PLATE, BarbarianAbilities.Rage);
+barbarian idiot = new barbarian(20, 1, 5, "GreatAxe", AvaliableArmour.PLATE, BarbarianAbilities.INTIMIDATE);
 cleric gaylord = new cleric(16, 18, 19, "ShortSword", AvaliableArmour.CHAINSHIRT, CSpells.REGENERATION);
 wizard merlin = new wizard(11, 20, 15, "Quarter_Staff", AvaliableArmour.PADDED, WSpells.BLACKHOLE);
 item excalibur = new item(100, AvaliableEnchantment.CERTAINDEATH);
