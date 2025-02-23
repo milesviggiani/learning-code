d76109e5ac85a41e33d86b720d5ce80522aee8ef
milesviggiani <milesviggiani@gmail.com>
2025-02-23 15:25:12 -0500

    more barb abilities

--- enums/barbarianAbilities.java
+++ enums/barbarianAbilities.java
@@ -1,5 +1,5 @@
 public enum Abilities{
-    CLEAVE(25), SMASH(40), RAGE(12);
+    CLEAVE(25), SMASH(40), RAGE(12), INTIMIDATE(-1), LOCKSMASH(-1), SHRUGOFF(-1), 
 
     double damage;
     Abilities(double damage){
